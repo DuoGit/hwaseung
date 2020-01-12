9cc34b8ef269e8f87d2eb214605e52e45986704d
Phat Vo Tan <vtphat19@gmail.com>
2020-01-12 14:23:18 +0700

    hello there

---
+++ precache-manifest.9cc6092e7476aa73f54c535e2e4e49d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b12ed4264b01ea20ac522452e5019cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c3ed747fc3ae8aed753",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "61ffd5c8753cf99208cc",
+    "url": "/static/js/2.d348ecb5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.d348ecb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c3ed747fc3ae8aed753",
+    "url": "/static/js/main.b9cf5e38.chunk.js"
+  },
+  {
+    "revision": "696942293a92ac3a2bb6",
+    "url": "/static/js/runtime-main.889ad4b6.js"
+  }
+]);
